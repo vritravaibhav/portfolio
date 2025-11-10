f0dc8ece1e12a64cd75b577d48cfd60474d42558
Divyanshu vaibhav <divaibhavyanshu@gmail.com>
2025-11-10 21:11:03 +0530

    added more data in experirience

--- lib/constatnts/strings.dart
+++ lib/constatnts/strings.dart
@@ -2,16 +2,25 @@ const String about =
     "I'm a passionate and results-oriented Flutter developer with a strong background in building beautiful and performant mobile applications. I have a proven ability to learn quickly and adapt to new technologies. I'm eager to contribute my skills and enthusiasm to a challenging and rewarding role.";
 
 const String outshadeExperience =
-    "During my internship at Outshade, I gained valuable experience in mobile app development using Flutter. I was responsible for developing and maintaining several features for the company's flagship app, and I had the opportunity to work on a variety of projects, from bug fixes to new feature development.";
+    "Collaborated on UI design using Flutter and Figma, integrated APIs, managed state, and resolved bugs. Gained hands-on experience with Firebase, added features to notifications";
 
 const String aypexExperience =
     "As a freelance Flutter developer for Aypex, I was responsible for the end-to-end development of a new mobile app. I worked closely with the client to gather requirements, design the UI/UX, and develop the app from scratch. I was also responsible for deploying the app to the Google Play Store and Apple App Store.";
 
 const String c4dexp =
-    "At Connect 4 Digital India, I'm responsible for developing and maintaining the company's mobile apps. I work with a team of developers to build new features, fix bugs, and improve the overall performance of the apps. I'm also responsible for mentoring junior developers and helping them to grow their skills.";
+    "Developed and integrated a custom solution using the WooCommerce REST API to enhance e-commerce functionalities and used BLoC state management for a better user experience within a Flutter application. Integrated the STRIPE SDK as a payment gateway. Developed and integrated WebRTC for peer-to-peer audio and video communication, ensuring secure and reliable data transmission using ICE, STUN, and TURN servers. Handled signaling using WebSockets. Developed and maintained mobile apps using Flutter and Figma. Designed a Firebase database to simplify CRUD operations.";
 
 const String instaCloneDec =
-    'A full-stack Instagram clone built with Flutter and Firebase. Features include user authentication, photo sharing, and a real-time chat system.';
+    'Developed a Flutter-based Instagram clone, a platform that enables users to share photos, follow, like, and comment on posts. Implemented user authentication and authorization using FireAuth technology. Designed and developed the frontend interface to closely mimic Instagram\'s user experience. Utilized Firebase to efficiently store and access user data, posts, and interactions. Technology Used: Flutter, Firebase, Dart.';
 
 const String webrtcdec =
-    'A multi-user video calling app built with WebRTC and Firebase. Features include real-time video and audio streaming, screen sharing, and text chat.';
+    'Developed a dynamic MultiUser Video Call system using WebRTC with Firebase for signaling. Implemented an algorithm where each new user creates a peer connections for old user and generates an offer that is added to the Firebase document. Existing users access the new offer from Firebase, generate an answer, and establish a connection with the new peer. Technology Used: Flutter, Firebase, Dart.';
+
+const String amazonCloneDec =
+    'A Flutter-based project using Express.js, Node.js, and MongoDB. Implemented authorization using JSON Web Token and Node.js. Built CRUD functionality using Express.js, Used MongoDB to store data. Technology Used: Node.js, Express.js, Dart, Flutter.';
+
+const String aamchaChalakDec =
+    'Developed a Aamcha auto ride-booking application allowing users to book rides, similar to Uber. Integrated OpenStreetMap API for polypoint tracking.';
+
+const String electromotionExperience =
+    "Developed and maintained more than seven Flutter applications independently, integrating Firebase and using Provider, BLoC and riverpod for state management. Designed and built the Krishigati App, enabling users to track their vehicle’s battery status, faults, and errors. Integrated OpenStreetMap API for polypoint tracking on Street View. Developed a full-fledged e-commerce platform for purchasing Krishigati-manufactured tractors and implemented an AI chatbot using Conversational Dialogflow. Built a Aamcha chalak ride-management app for captains, enabling ride acceptance, navigation assistance, and earnings tracking. Developed a Aamcha auto ride-booking application allowing users to book rides, similar to Uber and Rapido, incorporating real-time tracking and seamless payment integration.";
--- lib/screens/portfolio_screen.dart
+++ lib/screens/portfolio_screen.dart
@@ -70,6 +70,7 @@ class PortfolioScreen extends StatelessWidget {
           ),
           const SizedBox(height: 20),
           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               IconButton(
                 icon: const FaIcon(FontAwesomeIcons.github),
@@ -84,6 +85,63 @@ class PortfolioScreen extends StatelessWidget {
                       "https://www.linkedin.com/in/divyanshu-vaibhav-6a7965202/"));
                 },
               ),
+            ],
+          ),
+          const SizedBox(height: 30),
+          Text(
+            'Education',
+            style: Theme.of(context).textTheme.displaySmall,
+          ),
+          const SizedBox(height: 10),
+          Text(
+            'Bachelor of Engineering',
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+          Text(
+            'Panjab University',
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          Text(
+            '2020-2024',
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          const SizedBox(height: 30),
+          Text(
+            'Contact',
+            style: Theme.of(context).textTheme.displaySmall,
+          ),
+          const SizedBox(height: 10),
+          Text(
+            '+91 9576671336',
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+          Text(
+            'divaibhavyanshu@gmail.com',
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+          const SizedBox(height: 30),
+          Text(
+            'Skills',
+            style: Theme.of(context).textTheme.displaySmall,
+          ),
+          const SizedBox(height: 10),
+          Wrap(
+            spacing: 8.0,
+            runSpacing: 4.0,
+            children: [
+              Chip(label: Text('Flutter')),
+              Chip(label: Text('Dart')),
+              Chip(label: Text('Firebase')),
+              Chip(label: Text('Node.js')),
+              Chip(label: Text('Express.js')),
+              Chip(label: Text('MongoDB')),
+              Chip(label: Text('Git')),
+              Chip(label: Text('REST APIs')),
+              Chip(label: Text('Provider')),
+              Chip(label: Text('Bloc')),
+              Chip(label: Text('GetX')),
+              Chip(label: Text('SQL')),
+              Chip(label: Text('NoSQL')),
             ],
           ),
         ],
@@ -106,51 +164,44 @@ class PortfolioScreen extends StatelessWidget {
         ),
         const SizedBox(height: 30),
         Text(
-          'Skills',
-          style: Theme.of(context).textTheme.displaySmall,
-        ),
-        const SizedBox(height: 10),
-        Wrap(
-          spacing: 8.0,
-          runSpacing: 4.0,
-          children: [
-            Chip(label: Text('Flutter')),
-            Chip(label: Text('Dart')),
-            Chip(label: Text('Firebase')),
-            Chip(label: Text('Node.js')),
-            Chip(label: Text('Express.js')),
-            Chip(label: Text('MongoDB')),
-            Chip(label: Text('Git')),
-          ],
-        ),
-        const SizedBox(height: 30),
-        Text(
           'Experience',
           style: Theme.of(context).textTheme.displaySmall,
         ),
         const SizedBox(height: 10),
-        ExperienceCard(
-          imagePath: "dfsd",
-          title: "Internship",
-          companyName: "Outshade Digital Media",
-          description: outshadeExperience,
-          duration: "May 2024 - August 2024",
-        ),
-        const SizedBox(height: 20),
-        ExperienceCard(
-          imagePath: "dfsd",
-          title: "Freelance",
-          companyName: "Aypex pvt ltd",
-          description: aypexExperience,
-          duration: "October 2023 - currenly working",
-        ),
-        const SizedBox(height: 20),
-        ExperienceCard(
-          imagePath: "dfsd",
-          title: "Flutter Developer",
-          companyName: "Connect 4 digial India",
-          description: c4dexp,
-          duration: "April 2024 - currently working",
+        GridView.count(
+          crossAxisCount: 2,
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          children: [
+            ExperienceCard(
+              imagePath: "dfsd",
+              title: "Internship",
+              companyName: "Outshade Digital Media",
+              description: outshadeExperience,
+              duration: "May 2024 - August 2024",
+            ),
+            ExperienceCard(
+              imagePath: "dfsd",
+              title: "Freelance",
+              companyName: "Aypex pvt ltd",
+              description: aypexExperience,
+              duration: "October 2023 - currenly working",
+            ),
+            ExperienceCard(
+              imagePath: "dfsd",
+              title: "Flutter Developer",
+              companyName: "Connect 4 digial India",
+              description: c4dexp,
+              duration: "April 2024 - currently working",
+            ),
+            ExperienceCard(
+              imagePath: "dfsd",
+              title: "Flutter Developer",
+              companyName: "Electromotion E-vidyut",
+              description: electromotionExperience,
+              duration: "Placeholder duration",
+            ),
+          ],
         ),
         const SizedBox(height: 30),
         Text(
@@ -158,10 +209,31 @@ class PortfolioScreen extends StatelessWidget {
           style: Theme.of(context).textTheme.displaySmall,
         ),
         const SizedBox(height: 20),
-        ProjectCard(
-          projectName: 'Fullstack Instagram using Flutter and Firebase',
-          projectDescription:
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, quam et tempus pharetra, purus turpis tempus leo, quis pulvinar velit ex at odio.',
+        GridView.count(
+          crossAxisCount: 2,
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          children: [
+            CardItem(
+              time: "18 months ago",
+              heading: "Fullstack Instagram Clone using Flutter and Firebase",
+              title: 'Instagram with chat and Dating Option',
+              descriptiom: instaCloneDec,
+            ),
+            CardItem(
+              time: "15 months ago",
+              heading: 'Amazon Clone',
+              title: 'Amazon clone using Node.js',
+              descriptiom:
+                  'I developed a Flutter-based Amazon app using Express.js, Node.js, and MongoDB. The project features authorization with JSON Web Token and Node.js, and it uses MongoDB for data storage. I implemented CRUD operations in the database using Express.js. The project was built using Node.js, Express.js, Dart, and Flutter',
+            ),
+            CardItem(
+              time: "1 months ago",
+              heading: 'Multiuser video call using WEBRTC and firebase',
+              title: 'Multiuser video call',
+              descriptiom: webrtcdec,
+            )
+          ],
         ),
         const SizedBox(height: 30),
         Text(
@@ -173,54 +245,6 @@ class PortfolioScreen extends StatelessWidget {
         const SizedBox(height: 20),
         ContactUsContainer(),
       ],
-    );
-  }
-}
-
-class ProjectCard extends StatelessWidget {
-  final String projectName;
-  final String projectDescription;
-
-  const ProjectCard({
-    Key? key,
-    required this.projectName,
-    required this.projectDescription,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(16),
-      child: Column(
-        children: [
-          Wrap(
-            children: [
-              CardItem(
-                time: "18 months ago",
-                heading: "Fullstack Instagram Clone using Flutter and Firebase",
-                title: 'Instagram with chat and Dating Option',
-                descriptiom: instaCloneDec,
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              CardItem(
-                time: "15 months ago",
-                heading: 'Amazon Clone',
-                title: 'Amazon clone using Node.js',
-                descriptiom:
-                    'I developed a Flutter-based Amazon app using Express.js, Node.js, and MongoDB. The project features authorization with JSON Web Token and Node.js, and it uses MongoDB for data storage. I implemented CRUD operations in the database using Express.js. The project was built using Node.js, Express.js, Dart, and Flutter',
-              ),
-              CardItem(
-                time: "1 months ago",
-                heading: 'Multiuser video call using WEBRTC and firebase',
-                title: 'Multiuser video call',
-                descriptiom: webrtcdec,
-              )
-            ],
-          ),
-        ],
-      ),
     );
   }
 }
